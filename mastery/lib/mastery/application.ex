582f43c6962f5015a9933f59cf61be6de5cef9a4
Charles Roth <charlieroth4@icloud.com>
2020-08-08 13:00:28 -0400

    Application supervision tree

--- mastery/lib/mastery/application.ex
+++ mastery/lib/mastery/application.ex
@@ -1,12 +1,24 @@
 defmodule Mastery.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
-    children = []
+    IO.puts("Starting Mastery.Application...")
+    children = [
+      {
+        Mastery.Boundary.QuizManager,
+        [name: Mastery.Boundary.QuizManager]
+      },
+      {
+        Registry,
+        [name: Mastery.Registry.QuizSession, keys: :unique]
+      },
+      {
+        DynamicSupervisor,
+        [name: Mastery.Supervisor.QuizSession, strategy: :one_for_one]
+      }
+    ]
     opts = [strategy: :one_for_one, name: Mastery.Supervisor]
     Supervisor.start_link(children, opts)
   end
